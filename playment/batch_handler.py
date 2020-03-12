8bb7c2a17c25a82c4c5fa14c0c8937b960e0c033
pareekajay2 <pareekajay2@gmail.com>
2020-03-12 18:46:00 +0530

    changes made in project_handler and batch_handler

--- playment/batch_handler.py
+++ playment/batch_handler.py
@@ -4,16 +4,3 @@ class Batch:
         self.name = name
         self.label = label
         self.description = description
-
-    def get_batch_details(self):
-        # ToDo: An API to fetch further details
-        self.name = "batch_name"
-        self.label = "batch_label"
-        self.description = "batch_description"
-        self.created_at = "created_at"
-        self.updated_at = "updated_at"
-
-    def get_job_summary(self):
-        # ToDo: An API to fetch further details
-        summary = "api_response"
-        return summary
--- playment/project_handler.py
+++ playment/project_handler.py
@@ -1,28 +1,3 @@
-from playment.batch_handler import Batch
-
-
 class Project:
     def __init__(self, project_id: str):
         self.project_id = project_id
-        self.initialize_project_details()
-
-    def initialize_project_details(self):
-        # ToDo: An API call to fetch further details
-        self.name = "project_name"
-        self.label = "project_label"
-        self.created_at = "created_at"
-        self.updated_at = "updated_at"
-
-    def fetch_batch(self, batch_id: str):
-        return Batch(batch_id)
-
-    def get_summary(self):
-        # ToDo: An API call to fetch further details
-
-        summary = "api_response"
-        return summary
-
-    def get_batch_summary(self):
-        # ToDo: An API call to fetch further details
-        summary = "api_response"
-        return summary
