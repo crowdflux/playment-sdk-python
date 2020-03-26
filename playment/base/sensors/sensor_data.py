20d841206bef747775810d095884800584805e56
pareekajay2 <pareekajay2@gmail.com>
2020-03-26 18:02:16 +0530

    changes made as requested
    changes made as requested

---
+++ playment/base/sensors/sensor_data.py
@@ -0,0 +1,15 @@
+from playment.base.data import Data
+from playment.datatype_handler import SensorFusionData
+
+
+class SensorData(Data):
+    def __init__(self, sensor_fusion_data: SensorFusionData = SensorFusionData(), metadata=None):
+        self.sensor_data = sensor_fusion_data
+        self.metadata = metadata
+
+    def add_sensor_fusion_data(self, sensor_fusion_data: SensorFusionData):
+        assert type(sensor_fusion_data) is SensorFusionData
+        self.sensor_data = sensor_fusion_data
+
+    def valid(self):
+        pass
