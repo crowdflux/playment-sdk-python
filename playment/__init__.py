866bd8cbe6d8de641e874e4981f38d800fc7a8c9
pareekajay2 <pareekajay2@gmail.com>
2020-03-27 02:44:48 +0530

    changes made as in required

--- playment/__init__.py
+++ playment/__init__.py
@@ -2,5 +2,4 @@ from playment.batches import Batch
 from playment.exception import PlaymentException
 from playment.client_handler import Client
 from playment.base import *
-from playment.project_handler import Project
 from playment.jobs import Job
