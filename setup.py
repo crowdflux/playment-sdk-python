ae1cbe29192f2a5a10a7158abcf4a67475fdeff8
Ajay Pareek <39679557+pareekajay2@users.noreply.github.com>
2020-05-06 04:16:56 +0530

    Updated setup.py

--- setup.py
+++ setup.py
@@ -20,7 +20,6 @@ setuptools.setup(
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
     ],
     packages=setuptools.find_packages(),
