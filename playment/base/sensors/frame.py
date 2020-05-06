c79720c14dcd26c816b17f247c068a1f6adc5f9e
Ajay Pareek <39679557+pareekajay2@users.noreply.github.com>
2020-05-07 01:29:00 +0530

    Rectifying mutable errors (#12)
    * Change the version number to upload on pypi
    
    Change the version number to upload on pypi
    
    * Changes made as in required for use of nested folders in playment.
    
    * Changes made to rectify mutable errors in object.
    
    * Changes made to rectify mutable errors in object.
    
    * Updated Readme
    
    * Updated Readme
    
    * Changes made to rectify mutable errors in object.
    
    * Updated setup.py
    
    * Deleted .DS_Store
    
    * update setup.py
    
    * Update setup.py
    
    * update setup.py

--- playment/base/sensors/frame.py
+++ playment/base/sensors/frame.py
@@ -3,10 +3,12 @@ from typing import List
 
 
 class Frame:
-    def __init__(self, frame_id: str = None, sensors: List[SensorFrameObject] = []):
+    def __init__(self, frame_id: str = None, sensors: List[SensorFrameObject] = None):
         self.frame_id = frame_id
         self.sensors = sensors
 
     def add_sensor(self, sensor: SensorFrameObject):
         assert type(sensor) is SensorFrameObject
+        if self.sensors is None:
+            self.sensors = []
         self.sensors.append(sensor)
--- playment/base/sensors/sensor_fusion_data.py
+++ playment/base/sensors/sensor_fusion_data.py
@@ -4,12 +4,16 @@ from typing import List
 
 
 class SensorFusionData:
-    def __init__(self, frames: List[Frame] = [], sensor: List[Sensor] = []):
+    def __init__(self, frames: List[Frame] = None, sensor: List[Sensor] = None):
         self.frames = frames
         self.sensor_meta = sensor
 
     def add_sensor(self, sensor: Sensor):
+        if self.sensor_meta is None:
+            self.sensor_meta = []
         self.sensor_meta.append(sensor)
 
     def add_frame(self, frame: Frame):
+        if self.frames is None:
+            self.frames = []
         self.frames.append(frame)
